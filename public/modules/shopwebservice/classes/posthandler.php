34ef086b1d4530876b21ac779dd9524872fb6325
mistily <kutasi_beata@yahoo.com>
2021-03-10 16:37:45 +0200

    Insert written

--- public/modules/shopwebservice/classes/posthandler.php
+++ public/modules/shopwebservice/classes/posthandler.php
@@ -2,9 +2,12 @@
 
 namespace PrestaShop\Modules\ShopWebService\Classes;
 
+use PrestaShop\Modules\ShopWebService\Models\ProductsModel;
+
 class PostHandler {
 
   var $__catvals;
+  var $__postvals;
 
   function __construct(array $url = null) {
     if($url!==null) {
@@ -19,29 +22,8 @@ class PostHandler {
     }
     if(!empty($_POST)) {
       $this->createPostParamStruct($_POST);
+      $this->insertData(ucfirst(strtolower($_POST['type'])));
     }
-    // foreach ($this->__catvals as $key => $value) {
-    //   switch ($value[0]) {
-    //     case 'product':
-    //       $resp[] = $this->postValues($value,'Product');
-    //       break;
-    //     case 'carrier':
-    //       $resp[] = $this->postValues($value,'Carrier');
-    //       break;
-    //     case 'order':
-    //       $resp[] = $this->postValues($value, 'Order');
-    //       break;
-    //     case 'supplier':
-    //       $resp[] = $this->postValues($value, 'Supplier');
-    //       break;
-    //     case 'category':
-    //       $resp[] = $this->postValues($value, 'Categorie');
-    //       break;
-    //     default:
-    //       $resp[] = array("Request error!"=>"Request was not understood!");
-    //       break;
-    //   }
-    // }
     return $resp;
   }
 
@@ -56,7 +38,37 @@ class PostHandler {
   }
 
   private function createPostParamStruct($postvars) {
-    //create parameters for all models
+    $insertable = null;
+    if(isset($postvars['type'])) {
+      switch (strtolower($postvars['type'])) {
+        $tmpname = ucfirst(strtolower($postvars['type'])) . 'sModel';
+        $m = new $tmpname();
+        $tmpname = 'get' . ucfirst(strtolower($postvars['type'])) . 'sSchema';
+        $schema = $m->$tmpname();
+      }
+      foreach ($schema as $key => $value) {
+        if(isset($value['Field'])) {
+          if(in_array($_POST, $value['Field'])) {
+            $insertable[$value['Field']] = $_POST[$value['Field']];
+          }
+        }
+      }
+      $this->__postvals = $insertable;
+    } else {
+      $this->__postvals = array('Error!' => 'Please specify a type of resource!');
+    }
+  }
+
+  private function insertData($type) {
+    $tmpname = $type . 'sModel';
+    $m = new $tmpname();
+    $tmpname = 'insert' . $type;
+    try {
+      $m->$tmpname($this->__postvals);
+      return true;
+    } catch (Exception $e) {
+      return $e->getMessage();
+    }
   }
 }
  ?>
--- public/modules/shopwebservice/models/products.php
+++ public/modules/shopwebservice/models/products.php
@@ -6,16 +6,43 @@ use DB;
 class ProductsModel {
 
   function getAllProducts() {
-    return Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('SELECT * FROM ' . _DB_PREFIX_ . 'product');
+    try {
+      $sql = 'SELECT * FROM ' . _DB_PREFIX_ . 'product';
+      return Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
+    } catch(Exception $e) {
+      return $e->getMessage();
+    }
   }
 
   function getProductsSchema() {
-    return Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('DESCRIBE ' . _DB_PREFIX_ . 'product');
+    try {
+      $sql = 'DESCRIBE ' . _DB_PREFIX_ . 'product';
+      return Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
+    } catch(Exception $e) {
+      return $e->getMessage();
+    }
   }
 
   function getProductById(int $id) {
-    $sql = 'SELECT * FROM ' . _DB_PREFIX_;
-    $sql .= 'product WHERE id_product=' . $id;
-    return Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
+    try {
+      $sql = 'SELECT * FROM ' . _DB_PREFIX_;
+      $sql .= 'product WHERE id_product=' . $id;
+      return Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
+    } catch (Exception $e) {
+      return $e->getMessage();
+    }
+  }
+
+  function insertProduct(array $data) {
+    try {
+      $sql = 'INSERT INTO ' . _DB_PREFIX_  . '`product`( ';
+      $keys = implode(array_keys($data),',');
+      $sql .= $keys . ") VALUES (";
+      $values = implode(array_values($data));
+      $sql .= $values . ")";
+      return Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
+    } catch(Exception $e) {
+      return $e->getMessage();
+    }
   }
 }
